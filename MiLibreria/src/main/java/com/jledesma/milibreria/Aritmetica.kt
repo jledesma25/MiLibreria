378cccae7d37f2a7644fcbcb48fd9bf0cc2441a9
Juan Jose Ledesma <jledesma2509@gmail.com>
2021-05-09 17:18:38 -0500

    actualizacion

--- MiLibreria/src/main/java/com/jledesma/milibreria/Aritmetica.kt
+++ MiLibreria/src/main/java/com/jledesma/milibreria/Aritmetica.kt
@@ -2,23 +2,28 @@ package com.jledesma.milibreria
 
 class Aritmetica {
 
-    fun mas(numero1:Int,numero2:Int):Int{
-        return numero1 + numero2
-    }
+    companion object{
 
-    fun menos(numero1:Int,numero2:Int):Int{
-        return numero1 - numero2
-    }
+        fun mas(numero1:Int,numero2:Int):Int{
+            return numero1 + numero2
+        }
 
-    fun por(numero1:Int,numero2:Int):Int{
-        return numero1 * numero2
-    }
+        fun menos(numero1:Int,numero2:Int):Int{
+            return numero1 - numero2
+        }
 
-    fun entre(numero1:Int,numero2:Int):Int{
-        return if(numero2 == 0){
-            0
-        }else{
-            numero1 / numero2
+        fun por(numero1:Int,numero2:Int):Int{
+            return numero1 * numero2
+        }
+
+        fun entre(numero1:Int,numero2:Int):Int{
+            return if(numero2 == 0){
+                0
+            }else{
+                numero1 / numero2
+            }
         }
     }
+
+
 }
