2a881e53866203321429979d382d11d2205d0c5e
Juan Jose Ledesma <jledesma2509@gmail.com>
2021-05-09 16:46:21 -0500

    Carga libreria

---
+++ MiLibreria/src/main/java/com/jledesma/milibreria/Aritmetica.kt
@@ -0,0 +1,24 @@
+package com.jledesma.milibreria
+
+class Aritmetica {
+
+    fun mas(numero1:Int,numero2:Int):Int{
+        return numero1 + numero2
+    }
+
+    fun menos(numero1:Int,numero2:Int):Int{
+        return numero1 - numero2
+    }
+
+    fun por(numero1:Int,numero2:Int):Int{
+        return numero1 * numero2
+    }
+
+    fun entre(numero1:Int,numero2:Int):Int{
+        return if(numero2 == 0){
+            0
+        }else{
+            numero1 / numero2
+        }
+    }
+}
